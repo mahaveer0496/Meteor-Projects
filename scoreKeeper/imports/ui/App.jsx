b3688bb7f424621aae2003084fc484afdca08cd7
Mahaveer <mahaveersharma0496@yahoo.com>
2017-06-02 21:52:13 +0530

    Initial Commit Score Keeper

---
+++ scoreKeeper/imports/ui/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);    
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <p>Welcome to Score Keeper</p>
+      </div>
+    );
+  }
+}
+
+export default App;
